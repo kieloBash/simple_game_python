3b260d6daad9c76d8da1792a14b4d40939e04243
kieloBash <114970278+kieloBash@users.noreply.github.com>
2023-01-31 10:31:34 +0800

    added template for adding background color

--- simple_game.py
+++ simple_game.py
@@ -3,12 +3,24 @@ import pygame
 WIDTH, HEIGHT = 900, 500
 WIN = pygame.display.set_mode((WIDTH,HEIGHT))
 pygame.display.set_caption('First Game')
+
+BACKGROUND_COLOR = (255,255,255)
+
+
+def draw_window():
+    WIN.fill(BACKGROUND_COLOR)
+    pygame.display.update()
+
 def main():
     run = True
     while run:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+        draw_window()       
+                
+                
+                
     pygame.quit()
 
 if __name__ == "__main__":
