98ffa50f6f449c599932bc894534bb844ed20b81
kieloBash <114970278+kieloBash@users.noreply.github.com>
2023-01-31 10:27:57 +0800

    base template pygame for games

---
+++ simple_game.py
@@ -0,0 +1,15 @@
+import pygame
+
+WIDTH, HEIGHT = 900, 500
+WIN = pygame.display.set_mode((WIDTH,HEIGHT))
+pygame.display.set_caption('First Game')
+def main():
+    run = True
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+    pygame.quit()
+
+if __name__ == "__main__":
+    main() 
