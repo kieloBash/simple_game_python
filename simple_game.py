2492f5a5fa3f88ddc4e8c437e53df81005333e5b
kieloBash <114970278+kieloBash@users.noreply.github.com>
2023-02-01 20:08:42 +0800

    added score system

--- simple_game.py
+++ simple_game.py
@@ -1,10 +1,13 @@
 import pygame
+pygame.font.init()
+pygame.mixer.init()
 
 WIDTH, HEIGHT = 900, 500
 WIN = pygame.display.set_mode((WIDTH,HEIGHT))
 pygame.display.set_caption('First Game')
 
 BACKGROUND_COLOR = (255,255,255)
+WHITE = (255,255,255)
 BORDER_COLOR = (0,0,0)
 RED = (255,0,0)
 YELLOW = (255,255,0)
@@ -12,7 +15,7 @@ YELLOW = (255,255,0)
 FPS = 60
 VEL = 5
 BULLET_VEL = 7
-MAX_BULLETS = 10
+MAX_BULLETS = 3
 
 SPACESHIP_WIDTH, SPACESHIP_HEIGHT = 55,40
 
@@ -22,14 +25,26 @@ MIDDLE_BORDER = pygame.Rect((WIDTH/2)-10,0,10,HEIGHT)
 YELLOW_HIT = pygame.USEREVENT + 1
 RED_HIT = pygame.USEREVENT + 2
 
+HEALTH_FONT = pygame.font.SysFont('comicsans', 40)
+WINNER_FONT = pygame.font.SysFont('comicsans', 100)
+
 YELLOW_SPACESHIP_IMAGE = pygame.image.load('assets/spaceship_yellow.png')
 YELLOW_SPACESHIP = pygame.transform.rotate(pygame.transform.scale(YELLOW_SPACESHIP_IMAGE, (SPACESHIP_WIDTH, SPACESHIP_HEIGHT)),90)
 RED_SPACESHIP_IMAGE = pygame.image.load('assets/spaceship_red.png')
 RED_SPACESHIP = pygame.transform.rotate(pygame.transform.scale(RED_SPACESHIP_IMAGE, (SPACESHIP_WIDTH, SPACESHIP_HEIGHT)),270)
 
-def draw_window(red,yellow, red_bullets, yellow_bullets):
-    WIN.fill(BACKGROUND_COLOR)
+SPACE_BG = pygame.transform.scale(pygame.image.load('assets/space.png'), (WIDTH,HEIGHT))
+
+def draw_window(red,yellow, red_bullets, yellow_bullets,red_health,yellow_health):
+    WIN.blit(SPACE_BG,(0,0))
     pygame.draw.rect(WIN,BORDER_COLOR,MIDDLE_BORDER)
+    
+    red_health_text = HEALTH_FONT.render('Health: ' + str(red_health), 1, WHITE)
+    yellow_health_text = HEALTH_FONT.render('Health: ' + str(yellow_health), 1, WHITE)
+    
+    WIN.blit(red_health_text, (WIDTH - red_health_text.get_width() - 10, 10))
+    WIN.blit(yellow_health_text, (10, 10))
+    
     WIN.blit(YELLOW_SPACESHIP, (yellow.x, yellow.y))
     WIN.blit(RED_SPACESHIP, (red.x,red.y))
     
@@ -78,6 +93,12 @@ def handle_bullets(red_bullets, yellow_bullets, red,yellow):
         if bullet.x < 0:
             red_bullets.remove(bullet)
 
+def draw_winner(text):
+    draw_text = WINNER_FONT.render(text,1,WHITE)
+    WIN.blit(draw_text, (WIDTH/2 - draw_text.get_width() / 2, HEIGHT/2 - draw_text.get_height()/2))
+    pygame.display.update()
+    pygame.time.delay(1000)
+
 def main():
     
     red = pygame.Rect(700,MIDDLE,SPACESHIP_WIDTH,SPACESHIP_HEIGHT)
@@ -85,6 +106,9 @@ def main():
     
     red_bullets = [] 
     yellow_bullets = [] 
+    
+    red_health = 10
+    yellow_health = 10
     
     clock = pygame.time.Clock()
     run = True
@@ -95,13 +119,30 @@ def main():
                 run = False
 
             if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_LCTRL and len(yellow_bullets) < MAX_BULLETS:
+                if event.key == pygame.K_LALT and len(yellow_bullets) < MAX_BULLETS:
                     bullet = pygame.Rect(yellow.x + yellow.width,yellow.y + yellow.height//2 - 2, 10, 5)
                     yellow_bullets.append(bullet)
                     
                 if event.key == pygame.K_RCTRL and len(red_bullets) < MAX_BULLETS:
                     bullet = pygame.Rect(red.x,red.y + red.height//2 - 2, 10, 5)
                     red_bullets.append(bullet)
+                    
+            if event.type == RED_HIT:
+                red_health -= 1
+            
+            if event.type == YELLOW_HIT:
+                yellow_health -= 1
+                
+                
+        winner_text = ""
+        if yellow_health <= 0:
+            winner_text = "Yellow Wins!"
+            
+        if red_health <= 0:
+            winner_text = "Red Wins!"
+            
+        if winner_text != '':
+            draw_winner(winner_text)
         
         keys_pressed = pygame.key.get_pressed()
         yellow_handle_movement(keys_pressed,yellow)
@@ -109,7 +150,7 @@ def main():
         
         handle_bullets(red_bullets,yellow_bullets,red,yellow)
         
-        draw_window(red,yellow,red_bullets,yellow_bullets)       
+        draw_window(red,yellow,red_bullets,yellow_bullets,red_health,yellow_health)       
                 
                 
                 
